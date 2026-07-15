e208474f1b20d07ae97eded99e11c4560a341c68
axect <axect.tg@proton.me>
2026-07-15 14:54:04 +0900

    academic-slides: remove skill
    - skill directory (SKILL.md, assets/, references/) deleted; records the removal

--- academic-slides/assets/global-bottom.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer v-if="$slidev.nav.currentPage > 1" class="deck-footer">
-    <span class="dot" />
-    <span>{{ $slidev.configs.title }}</span>
-    <span class="spacer" />
-    <span>{{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}</span>
-  </footer>
-</template>
